f83d6171f801c99c63951ff865f94cd9a7e70a87
Andres Condezo <ra.condezo@gmail.com>
2021-08-11 11:33:41 -0500

    update: update save function in base model

--- models/base_model.py
+++ models/base_model.py
@@ -37,9 +37,8 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
