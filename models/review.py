fd029bd08e1fcb88a6c3ce4c848b86b0fa5a19c5
andres-condezo <ra.condezomonge@gmail.com>
2021-10-05 03:04:04 -0500

    Feat(review.py): [update review file]

--- models/review.py
+++ models/review.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+from sqlalchemy.sql.schema import ForeignKey
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
-from models.city import City
-from os import getenv
+from sqlalchemy import Column, String
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'reviews'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.is_db == 'db':
+        __tablename__ = 'reviews'
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
