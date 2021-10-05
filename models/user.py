d2aa5418485c1abbb158d197681eca1fec7b7705
andres-condezo <ra.condezomonge@gmail.com>
2021-10-05 03:01:18 -0500

    Feat(user.py): [update user file]

--- models/user.py
+++ models/user.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-
 from models.base_model import BaseModel, Base
-from os import getenv
-import models
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
 
-    if models.type_storage == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship('Place', backref='user')
-        reviews = relationship('Review', backref='user')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade='delete')
+    reviews = relationship('Review', backref='user', cascade='delete')
