af019ace024f651c7106535330431e74c7294ade
andres-condezo <ra.condezomonge@gmail.com>
2021-10-05 16:31:11 -0500

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity')
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary='place_amenity')
