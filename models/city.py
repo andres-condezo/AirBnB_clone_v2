41fb509dc9ebaade23a1376625fc3119e0f263a4
andres-condezo <ra.condezomonge@gmail.com>
2021-10-05 02:49:30 -0500

    Feat(city.py): [update city file]

--- models/city.py
+++ models/city.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+import models
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.is_db == "db":
+        __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete-orphan")
+        places = relationship('Place', backref='cities', cascade='delete')
     else:
         state_id = ""
         name = ""
